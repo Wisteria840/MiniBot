c2086f30123a0044ce60c6c3492106b6e945cf73
Remysaurus <48845480+Remysaurus@users.noreply.github.com>
2023-11-02 18:31:36 -0700

    TurnGoals

---
+++ MiniBot/src/main/java/frc/robot/commands/ZagZig.java
@@ -0,0 +1,69 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.lang.reflect.Array;
+
+import org.opencv.core.Mat;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.subsystems.Drivetrain;
+
+public class ZagZig extends CommandBase {
+  private static final class Config{
+    private static final double kRobotWidth = 30; /* width in inches */
+    private static final double kWheelDiameter = 12; /* in inches */
+    private static final int kTicksPerRevolution = 2048;
+
+    private static final double kP = 0.5;
+    private static final double kI = 0;
+    private static final double kD = 0;
+
+  }
+  /** Creates a new ZagZig. */
+  private Drivetrain m_driveTrain;
+  private PIDController m_pidController = new PIDController(Config.kP, Config.kI, Config.kD);
+  public ZagZig(Drivetrain drivetrain) {
+    m_driveTrain = drivetrain;
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  public static double [] getTurnGoals(double turnDegrees, double turnRadius, boolean left){
+    double insideGoal = (turnRadius) * (Math.toRadians(turnDegrees));
+    double outsideGoal = (turnRadius + Config.kRobotWidth) * (Math.toRadians(turnDegrees));
+    double[] turnGoals = {insideGoal, outsideGoal};
+    return turnGoals;
+
+
+  }
+
+  public static double toTicks(double distance){ /* input distance in inches output encoder ticks*/
+    return (distance/(Config.kWheelDiameter*Math.PI)) * Config.kTicksPerRevolution;
+  }
+}
+
---
+++ MiniBot/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase {
+  private static final class Config {
+    public static final int kRightPrimaryMotor = 1;
+    public static final int kRightSecondaryMotor = 2;
+    public static final int kLeftPrimaryMotor = 3;
+    public static final int kLeftSecondaryMotor = 4;
+  }
+
+  private WPI_TalonFX m_rightPrimary = new WPI_TalonFX(Config.kRightPrimaryMotor);
+  private WPI_TalonFX m_rightSecondary = new WPI_TalonFX(Config.kRightSecondaryMotor);
+  private WPI_TalonFX m_leftPrimary = new WPI_TalonFX(Config.kLeftPrimaryMotor);
+  private WPI_TalonFX m_leftSecondary = new WPI_TalonFX(Config.kLeftSecondaryMotor);
+
+
+
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {
+    m_leftSecondary.follow(m_leftPrimary);
+    m_rightSecondary.follow(m_rightPrimary);
+    m_rightPrimary.setInverted(true);
+    m_rightSecondary.setInverted(true);
+  }
+
+  public void setRightSpeed(double rightSpeed){
+    m_rightPrimary.set(rightSpeed);
+  }
+
+  public void setLeftSpeed(double leftSpeed) {
+    m_leftPrimary.set(leftSpeed);
+  }
+
+  public void setIdleMode(NeutralMode idleMode) {
+    m_leftPrimary.setNeutralMode(idleMode);
+    m_leftSecondary.setNeutralMode(idleMode);
+    m_rightPrimary.setNeutralMode(idleMode);
+    m_rightSecondary.setNeutralMode(idleMode);
+  }
+
+  public double getLeftPosition() {
+    return m_leftPrimary.getSelectedSensorPosition();
+  }
+
+  public double getRightPosition() {
+    return m_rightPrimary.getSelectedSensorPosition();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
